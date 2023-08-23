08d47971efca1b0a9667c2803c6f405dfef0e80a
aogieNch <131362056+aogieNch@users.noreply.github.com>
2023-08-23 11:06:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Này',
+    introDesc: `Anh biết mấy ngày nay công việc nhiều khiến em không vui, mà anh lại chẳng may làm em tức giận`,
+    btnIntro: 'Cho anh xin lỗi nhé',
+    title: 'Em có tha thứ cho anh không?',
+    desc: '',
+    btnYes: 'Vẫn thích anh',
+    btnNo: 'Không!!!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh',
+    btnReply: 'Gửi cho anh',
+    reply: 'Yêu thì yêu mà không yêu thì yêu',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé.',
+    btnAccept: 'Oki nho',
+    messLink: 'https://www.instagram.com/aogie.nch' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
